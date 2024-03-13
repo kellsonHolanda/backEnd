e03989854dc378f567b8de9a6f7be32f6a5e3223
kellsonHolanda <117248477+kellsonHolanda@users.noreply.github.com>
2024-03-13 16:19:15 -0300

    inclusão inicial de arquivos

---
+++ tarefaStream/src/Pessoa.java
@@ -0,0 +1,21 @@
+public class Pessoa {
+
+    
+        private String nome;
+        private String genero;
+    
+        public Pessoa(String nome, String genero) {
+            this.nome = nome;
+            this.genero = genero;
+        }
+    
+        public String getNome() {
+            return nome;
+        }
+    
+        public String getGenero() {
+            return genero;
+        }
+    
+    
+}
