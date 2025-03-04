f0c9ff3726243252ef5d346652f218486816c849
kellsonHolanda <117248477+kellsonHolanda@users.noreply.github.com>
2025-03-04 14:35:49 -0300

    Add files via upload

---
+++ Módulo 40/src/test/java/com/aluno/mod40/Mod40ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluno.mod40;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mod40ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
