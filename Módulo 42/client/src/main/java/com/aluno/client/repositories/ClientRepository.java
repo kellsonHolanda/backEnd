eee106cf0cd010283b284e5f7206a9dcf661bedf
kellsonHolanda <117248477+kellsonHolanda@users.noreply.github.com>
2025-03-05 11:24:15 -0300

    Add files via upload

---
+++ Módulo 42/client/src/main/java/com/aluno/client/repositories/ClientRepository.java
@@ -0,0 +1,11 @@
+package com.aluno.client.repositories;
+
+import com.aluno.client.core.domain.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, UUID> {
+}
